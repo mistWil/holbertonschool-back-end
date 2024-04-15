e7ab9cca8ed64527f54cb3c15e92719d5367e625
mistWil <7448@holbertonstudents.com>
2024-04-15 13:25:35 +0200

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,32 +11,47 @@ import requests
 
 
 def get_employee_todo_progress(employee_id):
-    # API endpoint URL
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    # API endpoint URL for employee information
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
 
-    # Send GET request to the API
-    response = requests.get(url)
+    # Send GET request to the API for employee information
+    response = requests.get(employee_url)
 
     # Check if the request was successful
     if response.status_code == 200:
         # Get the JSON data from the response
-        todos = response.json()
-
-        # Count the number of completed and total tasks
-        completed_tasks = [todo for todo in todos if todo["completed"]]
-        total_tasks = len(todos)
-        num_completed_tasks = len(completed_tasks)
+        employee_data = response.json()
 
         # Get the employee name
-        employee_name = todos[0]["title"].split(" ")[0]
+        employee_name = employee_data["name"]
 
-        # Print the employee TODO list progress
-        print(f"Employee {employee_name} is done with
-              tasks({num_completed_tasks}/{total_tasks}): ")
+        # API endpoint URL for employee's tasks
+        base_url = "https://jsonplaceholder.typicode.com/users/"
+        tasks_url = base_url + f"{employee_id}/todos"
 
-        # Print the titles of completed tasks
-        for task in reversed(completed_tasks):
-            print(f"\t {task['title']}")
+        # Send GET request to the API for employee's tasks
+        response = requests.get(tasks_url)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Get the JSON data from the response
+            todos = response.json()
+
+            # Count the number of completed and total tasks
+            completed_tasks = [todo for todo in todos if todo["completed"]]
+            total_tasks = len(todos)
+            num_completed_tasks = len(completed_tasks)
+
+            # Print the employee TODO list progress
+            employee_message = f"Employee {employee_name} is done with"
+            tasks_count = f"tasks({num_completed_tasks}/{total_tasks})"
+            print(employee_message + " " + tasks_count + ":")
+
+            # Print the titles of completed tasks
+            for task in reversed(completed_tasks):
+                print(f"\t {task['title']}")
+        else:
+            print(f"Error: {response.status_code}")
     else:
         print(f"Error: {response.status_code}")
 
